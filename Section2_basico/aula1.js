9e332fbbb1209743f3bd2d1d0a0e44955bf4af23
Davi Gabriel <davigabrielmota123@gmail.com>
2024-11-05 11:31:24 -0300

    Att aulas

--- Section2_basico/aula1.js
+++ Section2_basico/aula1.js
@@ -1,1 +1,5 @@
-console.log('Davi Gabriel');
+console.log("Davi 'Gabriel'"); // String = Texto
+console.log('Davi "Gabriel"'); // Number = Número
+console.log(`'Davi' "Gabriel"`);
+
+console.log(82, 15.75, "Davi Gabriel hits again");
---
+++ Section2_basico/aula2_ex.js
@@ -0,0 +1,1 @@
+console.log("Meu nome é 'Davi Gabriel'. Estou aprendendo JavaScript às", 11 ,"da manhã.");
